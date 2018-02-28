46953367f9aa3e33184407ce0d9223f9728ba543
Sebastian Peña <36645506+Sebascoman10@users.noreply.github.com>
2018-02-28 00:23:41 -0500

    Primer commit
    Archivos php

---
+++ ComprasPaypal/index.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<!--
+To change this license header, choose License Headers in Project Properties.
+To change this template file, choose Tools | Templates
+and open the template in the editor.
+-->
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title>Almacen productos</title>
+        <link rel="stylesheet" type="text/css" href="./css/estilos.css">
+	<script type="text/javascript"  href="./js/scripts.js"></script>
+</head>
+<body>
+	<header>
+		<h1>Compras</h1>
+		<a href="./carritodecompras.php" title="ver carrito de compras">
+			<img src="./imagenes/carrito.png">
+		</a>
+	</header>
+	<section>
+		
+	<?php
+		include 'conexion.php';
+		$re=mysql_query("select * from productos")or die(mysql_error());
+		while ($f=mysql_fetch_array($re)) {
+		?>
+			<div class="producto">
+			<center>
+				<img src="./productos/<?php echo $f['imagen'];?>"><br>
+				<span><?php echo $f['nombre'];?></span><br>
+				<a href="./detalles.php">ver</a>
+			</center>
+		</div>
+	<?php
+		}
+	?>	
+	</section>
+</body>
+</html>
